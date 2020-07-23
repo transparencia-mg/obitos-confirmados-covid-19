6962f3da461b90fc5f2629e551e6741b3529dc98
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2020-07-23 12:26:48 -0300

    Refatora script clean.R

--- scripts/clean.R
+++ scripts/clean.R
@@ -1,13 +1,12 @@
-library(dplyr)
+library(magrittr)
 
 source("scripts/lib/utils.R")
 
-#======================================================================
-# config
-path <- "data-raw/OBITOS_CONF_COVID-19_MG.xlsx"
+input <- "data-raw/OBITOS_CONF_COVID-19_MG.xlsx"
 output <- jsonlite::read_json("datapackage.json")$resources[[1]]$path
-#======================================================================
 
-obitos_confirmados_cge <- read(path) %>% recode()
+result <- input %>%
+            read() %>% 
+            recode()
 
-obitos_confirmados_cge %>% readr::write_excel_csv2(output)
+readr::write_excel_csv2(result, path = output)
