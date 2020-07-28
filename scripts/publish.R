39743029ecbec76941e2cb4288672e4a3f601586
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2020-07-28 12:34:52 -0300

    Altera id do recurso para nova instancia CKAN

--- scripts/publish.R
+++ scripts/publish.R
@@ -8,7 +8,7 @@ source_python("scripts/lib/utils.py")
 output <- jsonlite::read_json("datapackage.json")$resources[[1]]$path
 
 res <- upload_resource(output, 
-                       resource_id = "42fd644d-6a50-4539-a825-05b2460adfa0", 
+                       resource_id = "e5732e33-306f-422c-87b7-257c2e283de1", 
                        url = "http://dados.mg.gov.br", 
                        key = Sys.getenv("DADOSMG_PROD"))
 
