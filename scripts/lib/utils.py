d83b077ebb9b9355fdb7e842ae03120afe1dffad
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2020-07-31 15:53:32 -0300

    Substitui cliente python ckanapi por cliente ckanr

--- scripts/lib/utils.py
+++
@@ -1,7 +0,0 @@
-import ckanapi
-
-def upload_resource(file, resource_id, key, url):
-    ckan = ckanapi.RemoteCKAN(url, apikey = key)
-    resource = ckan.action.resource_show(id = resource_id)
-    res = ckan.action.resource_update(id = resource_id, upload = open(file,'rb'), format=resource["format"])
-    return res
--- scripts/publish.R
+++ scripts/publish.R
@@ -1,14 +1,9 @@
-library(reticulate)
-
 source("scripts/lib/utils.R")
-
-use_virtualenv("r-reticulate", required = TRUE)
-source_python("scripts/lib/utils.py")
 
 output <- jsonlite::read_json("datapackage.json")$resources[[1]]$path
 
-res <- upload_resource(output, 
-                       resource_id = "e5732e33-306f-422c-87b7-257c2e283de1", 
+res <- ckanr::resource_update(id = "e5732e33-306f-422c-87b7-257c2e283de1",
+                       path = output, 
                        url = "http://dados.mg.gov.br", 
                        key = Sys.getenv("DADOSMG_PROD"))
 
